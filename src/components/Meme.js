74d6eb37d4ffebb3a43e9830927ca7318e8ceef8
CJCB77 <coral.johnny.2@gmail.com>
2022-01-23 13:48:17 -0500

    Now load random meme template when changing React.state

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,15 +1,34 @@
 import React from "react"
-import ReactDOM from "react-dom"
 import "../style.css"
+import { memeTemplates } from "../templates"
+
 
 
 export default function Meme() {
+
     
+    const [meme, setMeme] = React.useState("")
+    
+    function getMeme(){
+        let memes =[]
+        for(let meme of memeTemplates){
+            memes.push(meme.url)
+        }
+        let index = Math.floor(Math.random() * memes.length)
+        let randMeme = memes[index] 
+        setMeme(randMeme)
+    }
+
     return(
-        <section className="generator">
-                <input type="text" placeholder="Shut up"/>
-                <input type="text" placeholder="and take my money"/>
-                <button type="" >Get a new meme image</button>
-        </section>
+        <React.Fragment>
+            <section className="generator">
+                    <input type="text" placeholder="Shut up"/>
+                    <input type="text" placeholder="and take my money"/>
+                    <button type="" id="btn" onClick={getMeme}>Get meme</button>
+            </section>
+            <div className="meme-container">
+                <img src={meme} alt="" className="template" id="template"/>
+            </div>
+        </React.Fragment>
     )
 }
--- src/index.js
+++ src/index.js
@@ -3,18 +3,18 @@ import ReactDOM from "react-dom"
 import "./style.css"
 import Navbar from "./components/Navbar"
 import Meme from "./components/Meme"
-import Container from "./components/Container"
-
-
 
 function App() {
+
     return (
+
         <React.Fragment>
             <Navbar />
             <Meme />
-            <Container />
         </React.Fragment>
     )
 }
+
+
 
 ReactDOM.render(<App />, document.getElementById("root"))
